be402f7aa702b04f1474d61494fc183505961f85
FootKjott <oscarmccully@gmail.com>
2015-02-07 00:39:26 -0500

    rename capture methods

--- ext/futscript/screen_ext/screen_ext.c
+++ ext/futscript/screen_ext/screen_ext.c
@@ -4,20 +4,24 @@
 VALUE Futscript = Qnil;
 VALUE Screen = Qnil;
 VALUE Image = Qnil;
+VALUE Color = Qnil;
 HDC user_hdc = NULL;
 
 void Init_screen_ext();
 VALUE get_pixel(VALUE self, VALUE x, VALUE y);
 VALUE get_pixel_wrapper(VALUE self, VALUE x, VALUE y);
+VALUE screen_capture_area(VALUE self, VALUE v_x, VALUE v_y, VALUE v_width, VALUE v_height);
 
 void Init_screen_ext() {
 	Futscript = rb_define_module("Futscript");
 	Screen = rb_define_class_under(Futscript, "Screen", rb_cObject);
 	Image = rb_define_class_under(Futscript, "Image", rb_cObject);
+	Color = rb_define_class_under(Futscript, "Color", rb_cObject);
 	user_hdc = CreateDC("DISPLAY", NULL, NULL, NULL);
 
 	rb_define_module_function(Screen, "get_pixel_ext", get_pixel_wrapper, 2);
-
+	rb_define_module_function(Screen, "capture_area", screen_capture_area, 4);
+	
 	rb_cv_set(Screen, "@@offset_x", GetSystemMetrics(76));
 	rb_cv_set(Screen, "@@offset_y", GetSystemMetrics(77));
 	rb_cv_set(Screen, "@@width", GetSystemMetrics(78));
@@ -25,12 +29,17 @@ void Init_screen_ext() {
 }
 
 VALUE get_pixel_wrapper(VALUE self, VALUE x, VALUE y) {
-	return INT2NUM(GetPixel(user_hdc, NUM2INT(x), NUM2INT(y)));
+	COLORREF cr = GetPixel(user_hdc, NUM2INT(x), NUM2INT(y));
+	VALUE argv[] = { INT2NUM(cr % 256), INT2NUM((cr / 256) % 256), INT2NUM((cr / 65536) % 256) };
+
+	return rb_class_new_instance(3, argv, Color);
 }
 
 
-VALUE capture_screen_area(VALUE self, VALUE x, VALUE y, VALUE width, VALUE height) {
+VALUE screen_capture_area(VALUE self, VALUE v_x, VALUE v_y, VALUE v_width, VALUE v_height) {
 	//TODO: fix conversions for parameters
+	int x = NUM2INT(v_x), y = NUM2INT(v_y);
+	int width = NUM2INT(v_width), height = NUM2INT(v_height);
 	BITMAPINFO bmi;
 	char* data;
 	VALUE argv[4];
@@ -53,8 +62,8 @@ VALUE capture_screen_area(VALUE self, VALUE x, VALUE y, VALUE width, VALUE heigh
 	GetDIBits(hdc_dest, h_bitmap, 0, height, data, &bmi, DIB_RGB_COLORS);
 	DeleteObject(h_bitmap);
 	DeleteDC(hdc_dest);
-	argv[0] = width;
-	argv[1] = height; 
+	argv[0] = v_width;
+	argv[1] = v_height; 
 	argv[2] = INT2NUM(bmi.bmiHeader.biSizeImage);
 	argv[3] = rb_str_new(data, bmi.bmiHeader.biSizeImage);
 	return rb_class_new_instance(4, argv, Image);
--- lib/futscript/screen.rb
+++ lib/futscript/screen.rb
@@ -2,22 +2,6 @@ require 'futscript/screen_ext/screen_ext'
 
 module Futscript
   class Screen
-    @@CreateDC = Win32API.new('gdi32', 'CreateDC', ['S', 'S', 'S', 'I'], 'I')  
-    @@CreateCompatibleDC = Win32API.new('gdi32', "CreateCompatibleDC", ["I"], "I")
-    @@CreateCompatibleBitmap = Win32API.new('gdi32', "CreateCompatibleBitmap", ["I", "I", "I"], "I")
-    @@SelectObject = Win32API.new('gdi32', "SelectObject", ["I", "I"], "I")
-    @@BitBlt = Win32API.new('gdi32', "BitBlt", ["I", "I", "I", "I", "I", "I", "I", "I", "I"], "I")
-    @@DeleteDC = Win32API.new('gdi32', "DeleteDC", ["I"], "I")
-    @@GetDIBits = Win32API.new('gdi32', 'GetDIBits', ["I", "I", "I", "I", "P", "P" , "I"], "I")
-    @@DeleteObject = Win32API.new('gdi32', 'DeleteObject', ["I"], "I")
-    @@GetSystemMetrics = Win32API.new('user32', 'GetSystemMetrics', ["I"], "I")
-
-    @@hdc = @@CreateDC.call("DISPLAY", nil, nil, 0)
-    @@offset_x = @@GetSystemMetrics.call(76)
-    @@offset_y = @@GetSystemMetrics.call(77)
-    @@width = @@GetSystemMetrics.call(78)
-    @@height = @@GetSystemMetrics.call(79)
-
     def self.offset_x
       return @@offset_x
     end
@@ -34,35 +18,17 @@ module Futscript
       return @@height
     end
 
-    def self.capture_all_screens
-      capture_screen_area 0, 0, @@width, @@height
-    end
-
-    def self.capture_screen_area x, y, width, height
-      hdc_dest = @@CreateCompatibleDC.call(@@hdc)
-      h_bitmap = @@CreateCompatibleBitmap.call(@@hdc.to_i, width, height)
-      @@SelectObject.call(hdc_dest, h_bitmap)
-      @@BitBlt.call(hdc_dest, 0, 0, width, height, @@hdc, x + @@offset_x, y + @@offset_y, 0x40000000 | 0x00CC0020)
-      bmi = [40, width, height, 1, 24].pack("LllSS").ljust(44, "\0")
-      @@GetDIBits.call(hdc_dest, h_bitmap, 0, height, nil, bmi, 0x00) #Sets BITMAPINFO bmi
-      bmiarr = bmi.unpack("LllSSLLllLLCCCC")
-      bmpbuffer = "\0" * bmiarr[6]
-      @@GetDIBits.call(hdc_dest, h_bitmap, 0, height, bmpbuffer, bmi, 0x00) #Fills bmpbuffer
-      @@DeleteDC.call(hdc_dest)
-      @@DeleteObject.call(h_bitmap)
-      return Image.from_bmi_data(bmiarr, bmpbuffer)
+    def self.capture
+      capture_area 0, 0, @@width, @@height
     end
 
     def self.get_pixel x, y
       raise "Invalid coordinates" unless (0...@@width).include?(x) && (0...@@height).include?(y)
-      colorref = get_pixel_ext x, y
-      return Color.new(colorref % 256, (colorref / 256) % 256, (colorref / 65536) % 256)
+      return get_pixel_ext x, y
     end
 
     def self.wait_for_px x, y, color, tolerance=5, timeout=100, ms_per_screenshot=50, is=true
-      puts color.inspect
       color = Color.parse color
-      puts color.inspect
       start_time = Time.now.to_i
       until is == color.is_tolerant_of(get_pixel(x, y), tolerance) do
         return false if Time.now.to_i - start_time >= timeout
@@ -75,13 +41,8 @@ module Futscript
       wait_for_px x, y, color, tolerance, timeout, ms_per_screenshot, false
     end
 
-    def self.lel
-      puts 'lel'
-    end
-
     def self.method_missing(m, *args, &block)
-      puts 'missing'
-      capture_all_screens.send(m, *args)
+      capture.send(m, *args)
     end
   end
 end
